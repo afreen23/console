58c7e916002efbf0f722b12f1df1847e43ca6d4c
Kanika <kmurarka@redhat.com>
2019-10-18 13:05:57 +0530

    Loading component
    Signed-off-by: Kanika <kmurarka@redhat.com>

--- frontend/packages/ceph-storage-plugin/src/components/dashboard-page/storage-dashboard/breakdown-card/breakdown-loading.tsx
+++ frontend/packages/ceph-storage-plugin/src/components/dashboard-page/storage-dashboard/breakdown-card/breakdown-loading.tsx
@@ -5,23 +5,18 @@ import './breakdown-card.scss';
 export const BreakdownChartLoading: React.FC<{}> = () => {
   return (
     <>
-      <Grid className="skeleton-stack-available-bar-box">
-        <GridItem lg={4} md={4} sm={4} className="skeleton-stack-available-bar" />
-        <GridItem lg={4} md={4} sm={4} />
-        <GridItem lg={4} md={4} sm={4} className="skeleton-stack-available-bar" />
+      <Grid className="skeleton-box">
+        <GridItem span={4} className="skeleton-activity" />
+        <GridItem span={4} className="skeleton-activity skeleton-stack-available-bar" />
+        <GridItem span={4} className="skeleton-activity" />
+        <GridItem span={12} className="skeleton-activity skeleton-stack-bar" />
+        <GridItem span={2} className="skeleton-activity skeleton-stack-legend" />
+        <GridItem span={2} className="skeleton-activity skeleton-stack-legend" />
+        <GridItem span={2} className="skeleton-activity skeleton-stack-legend" />
+        <GridItem span={2} className="skeleton-activity skeleton-stack-legend" />
+        <GridItem span={2} className="skeleton-activity skeleton-stack-legend" />
+        <GridItem span={2} className="skeleton-activity skeleton-stack-legend" />
       </Grid>
-      <Grid className="skeleton-stack-bar-box">
-        <GridItem lg={12} md={12} sm={12} className="skeleton-stack-bar" />
-      </Grid>
-      <Grid gutter="sm" sm={4} className="skeleton-stack-available-bar-box">
-        <GridItem lg={2} md={4} sm={4} className="skeleton-stack-legend" />
-        <GridItem lg={2} md={4} sm={4} className="skeleton-stack-legend" />
-        <GridItem lg={2} md={4} sm={4} className="skeleton-stack-legend" />
-        <GridItem lg={2} md={4} sm={4} className="skeleton-stack-legend" />
-        <GridItem lg={2} md={4} sm={4} className="skeleton-stack-legend" />
-        <GridItem lg={2} md={4} sm={4} className="skeleton-stack-legend" />
-      </Grid>
-      <Grid />
     </>
   );
 };
